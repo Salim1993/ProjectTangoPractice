433645abb14a0b0e94574fdc9f3d99a0d784c9de
Salim Ben Khaled <salimbenkhaled93@gmail.com>
2016-05-31 10:58:23 -0600

    Added Tango features. Is now up to level of the Augmented reality example

--- app/src/main/java/com/example/salim/my_tango_test/MyTangoTestActivity.java
+++ app/src/main/java/com/example/salim/my_tango_test/MyTangoTestActivity.java
@@ -3,33 +3,276 @@ package com.example.salim.my_tango_test;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+
+import org.rajawali3d.scene.ASceneFrameCallback;
 import org.rajawali3d.surface.IRajawaliSurface;
 import org.rajawali3d.surface.RajawaliSurfaceView;
 
+import com.google.atap.tangoservice.TangoXyzIjData;
+import com.projecttango.rajawali.DeviceExtrinsics;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * This is just test code to understand Rajawali and OpenGL, and how to
- * work with Google's project tango. 
+ * work with Google's project tango. Code used from Tango examples, specifically AugmentedReality example
+ *
+ * Currently it doesn't do anything new or different compared to the Augmented reality example
  */
 
 public class MyTangoTestActivity extends AppCompatActivity {
+    private static final String TAG = MyTangoTestActivity.class.getSimpleName();
+    private static final TangoCoordinateFramePair FRAME_PAIR = new TangoCoordinateFramePair(
+            TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+            TangoPoseData.COORDINATE_FRAME_DEVICE);
+    private static final int INVALID_TEXTURE_ID = 0;
 
-    myRenderer renderer;
+    private RajawaliSurfaceView surface;
+    private myRenderer mRenderer;
+    private Tango mTango;
+    //private TangoConfig mConfig;
+    private TangoCameraIntrinsics mIntrinsics;
+    private DeviceExtrinsics mExtrinsics;
+    private boolean mIsConnected = false;
+    private double mCameraPoseTimestamp = 0;
+
+    // Texture rendering related fields
+    // NOTE: Naming indicates which thread is in charge of updating this variable
+    private int mConnectedTextureIdGlThread = INVALID_TEXTURE_ID;
+    private AtomicBoolean mIsFrameAvailableTangoThread = new AtomicBoolean(false);
+    private double mRgbTimestampGlThread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tango_test);
 
-        final RajawaliSurfaceView surface = new RajawaliSurfaceView(this);
+        surface = new RajawaliSurfaceView(this);
         surface.setFrameRate(60.0);
         surface.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
 
-        //how is this different from setContentView()
+        //how is this different from setContentView(), is it cause of setContentView(R.layout.activity_my_tango_test);
         addContentView(surface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
 
-        renderer = new myRenderer(this);
-        surface.setSurfaceRenderer(renderer);
+        mRenderer = new myRenderer(this);
+        surface.setSurfaceRenderer(mRenderer);
+
+        mTango = new Tango(this);
+
+        /*
+        mConfig = new TangoConfig();
+        mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_COLORCAMERA, true);
+        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_LOWLATENCYIMUINTEGRATION, true);
+        */
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Synchronize against disconnecting while the service is being used in the OpenGL thread or
+        // in the UI thread.
+        if (!mIsConnected) {
+            // Initialize Tango Service as a normal Android Service, since we call
+            // mTango.disconnect() in onPause, this will unbind Tango Service, so
+            // every time when onResume get called, we should create a new Tango object.
+            mTango = new Tango(MyTangoTestActivity.this, new Runnable() {
+                // Pass in a Runnable to be called from UI thread when Tango is ready,
+                // this Runnable will be running on a new thread.
+                // When Tango is ready, we can call Tango functions safely here only
+                // when there is no UI thread changes involved.
+                @Override
+                public void run() {
+                    try {
+                        connectTango();
+                        setupRenderer();
+                        mIsConnected = true;
+                    } catch (TangoOutOfDateException e) {
+                        Log.e(TAG, "Tango service outdated", e);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Synchronize against disconnecting while the service is being used in the OpenGL thread or
+        // in the UI thread.
+        // NOTE: DO NOT lock against this same object in the Tango callback thread. Tango.disconnect
+        // will block here until all Tango callback calls are finished. If you lock against this
+        // object in a Tango callback thread it will cause a deadlock.
+        synchronized (this) {
+            if (mIsConnected) {
+                mIsConnected = false;
+                mRenderer.getCurrentScene().clearFrameCallbacks();
+                mTango.disconnectCamera(TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+                // We need to invalidate the connected texture ID so that we cause a re-connection
+                // in the OpenGL thread after resume
+                mConnectedTextureIdGlThread = INVALID_TEXTURE_ID;
+                mTango.disconnect();
+            }
+        }
+    }
+
+    /**
+     * Configures the Tango service and connect it to callbacks.
+     */
+    private void connectTango() {
+        // Use default configuration for Tango Service, plus color camera and
+        // low latency IMU integration.
+        TangoConfig config = mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_COLORCAMERA, true);
+        // NOTE: Low latency integration is necessary to achieve a precise alignment of
+        // virtual objects with the RBG image and produce a good AR effect.
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_LOWLATENCYIMUINTEGRATION, true);
+        mTango.connect(config);
+
+        // No need to add any coordinate frame pairs since we are not using pose data from callbacks
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+
+        mTango.connectListener(framePairs, new Tango.OnTangoUpdateListener() {
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                // We are not using onPoseAvailable for this app.
+            }
+
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
+                // We are not using onXyzIjAvailable for this app.
+            }
+
+            @Override
+            public void onTangoEvent(TangoEvent event) {
+                // We are not using onTangoEvent for this app.
+            }
+
+            @Override
+            public void onFrameAvailable(int cameraId) {
+                // Check if the frame available is for the camera we want and update its frame
+                // on the view.
+                if (cameraId == TangoCameraIntrinsics.TANGO_CAMERA_COLOR) {
+                    // Mark a camera frame is available for rendering in the OpenGL thread
+                    mIsFrameAvailableTangoThread.set(true);
+                    // Trigger an Rajawali render to update the scene with the new RGB data.
+                    surface.requestRender();
+                }
+            }
+
+
+        });
+
+        // Get extrinsics from device for use in transforms. This needs
+        // to be done after connecting Tango and listeners.
+        mExtrinsics = setupExtrinsics(mTango);
+        mIntrinsics = mTango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+    }
+
+    /**
+     * Connects the view and renderer to the color camara and callbacks.
+     */
+    private void setupRenderer() {
+
+        // Register a Rajawali Scene Frame Callback to update the scene camera pose whenever a new
+        // RGB frame is rendered.
+        // (@see https://github.com/Rajawali/Rajawali/wiki/Scene-Frame-Callbacks)
+        mRenderer.getCurrentScene().registerFrameCallback(new ASceneFrameCallback() {
+            @Override
+            public void onPreFrame(long sceneTime, double deltaTime) {
+                // NOTE: This is called from the OpenGL render thread, after all the renderer
+                // onRender callbacks had a chance to run and before scene objects are rendered
+                // into the scene.
+
+                // Prevent concurrent access to {@code mIsFrameAvailableTangoThread} from the Tango
+                // callback thread and service disconnection from an onPause event.
+                synchronized (MyTangoTestActivity.this) {
+                    // Don't execute any tango API actions if we're not connected to the service
+                    if (!mIsConnected) {
+                        return;
+                    }
+
+                    // Set-up scene camera projection to match RGB camera intrinsics
+                    if (!mRenderer.isSceneCameraConfigured()) {
+                        mRenderer.setProjectionMatrix(mIntrinsics);
+                    }
+
+                    // Connect the camera texture to the OpenGL Texture if necessary
+                    // NOTE: When the OpenGL context is recycled, Rajawali may re-generate the
+                    // texture with a different ID.
+                    if (mConnectedTextureIdGlThread != mRenderer.getTextureId()) {
+                        mTango.connectTextureId(TangoCameraIntrinsics.TANGO_CAMERA_COLOR,
+                                mRenderer.getTextureId());
+                        mConnectedTextureIdGlThread = mRenderer.getTextureId();
+                        Log.d(TAG, "connected to texture id: " + mRenderer.getTextureId());
+                    }
+
+                    // If there is a new RGB camera frame available, update the texture with it
+                    if (mIsFrameAvailableTangoThread.compareAndSet(true, false)) {
+                        mRgbTimestampGlThread =
+                                mTango.updateTexture(TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+                    }
+
+                    // If a new RGB frame has been rendered, update the camera pose to match.
+                    if (mRgbTimestampGlThread > mCameraPoseTimestamp) {
+                        // Calculate the device pose at the camera frame update time.
+                        TangoPoseData lastFramePose = mTango.getPoseAtTime(mRgbTimestampGlThread,
+                                FRAME_PAIR);
+                        if (lastFramePose.statusCode == TangoPoseData.POSE_VALID) {
+                            // Update the camera pose from the renderer
+                            mRenderer.updateRenderCameraPose(lastFramePose, mExtrinsics);
+                            mCameraPoseTimestamp = lastFramePose.timestamp;
+                        } else {
+                            Log.w(TAG, "Can't get device pose at time: " + mRgbTimestampGlThread);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onPreDraw(long sceneTime, double deltaTime) {
+
+            }
+
+            @Override
+            public void onPostFrame(long sceneTime, double deltaTime) {
+
+            }
+
+            @Override
+            public boolean callPreFrame() {
+                return true;
+            }
+        });
+    }
+
+    private static DeviceExtrinsics setupExtrinsics(Tango tango) {
+        // Create camera to IMU transform.
+        TangoCoordinateFramePair framePair = new TangoCoordinateFramePair();
+        framePair.baseFrame = TangoPoseData.COORDINATE_FRAME_IMU;
+        framePair.targetFrame = TangoPoseData.COORDINATE_FRAME_CAMERA_COLOR;
+        TangoPoseData imuTrgbPose = tango.getPoseAtTime(0.0, framePair);
+
+        // Create device to IMU transform.
+        framePair.targetFrame = TangoPoseData.COORDINATE_FRAME_DEVICE;
+        TangoPoseData imuTdevicePose = tango.getPoseAtTime(0.0, framePair);
+
+        // Create depth camera to IMU transform.
+        framePair.targetFrame = TangoPoseData.COORDINATE_FRAME_CAMERA_DEPTH;
+        TangoPoseData imuTdepthPose = tango.getPoseAtTime(0.0, framePair);
+
+        return new DeviceExtrinsics(imuTdevicePose, imuTrgbPose, imuTdepthPose);
     }
 }
--- app/src/main/java/com/example/salim/my_tango_test/myRenderer.java
+++ app/src/main/java/com/example/salim/my_tango_test/myRenderer.java
@@ -5,26 +5,44 @@ import android.util.Log;
 import android.view.MotionEvent;
 
 import com.example.salim.my_tango_test.R;
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.projecttango.rajawali.DeviceExtrinsics;
+import com.projecttango.rajawali.Pose;
+import com.projecttango.rajawali.ScenePoseCalculator;
 
+import org.rajawali3d.Object3D;
 import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.materials.Material;
 import org.rajawali3d.materials.methods.DiffuseMethod;
 import org.rajawali3d.materials.methods.SpecularMethod;
 import org.rajawali3d.materials.textures.ATexture;
+import org.rajawali3d.materials.textures.StreamingTexture;
 import org.rajawali3d.materials.textures.Texture;
+import org.rajawali3d.math.Matrix4;
 import org.rajawali3d.math.vector.Vector3;
+import org.rajawali3d.primitives.ScreenQuad;
 import org.rajawali3d.primitives.Sphere;
 import org.rajawali3d.renderer.RajawaliRenderer;
+
+import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created by salim on 25/05/16.
  */
 public class myRenderer extends RajawaliRenderer {
+    private static final String TAG = myRenderer.class.getSimpleName();
 
-    //tutorial says we need a context, but we dont need it it since it is never used
+    //tutorial says we need a context, but we don't need it it since it is never used
     private Context context;
     private DirectionalLight directionalLight;
     private Sphere earthSphere;
+
+    // Rajawali texture used to render the Tango color camera
+    private ATexture mTangoCameraTexture;
+
+    // Keeps track of whether the scene camera has been configured
+    private boolean mSceneCameraConfigured;
 
 
     public myRenderer(Context context) {
@@ -36,6 +54,26 @@ public class myRenderer extends RajawaliRenderer {
 
     @Override
     protected void initScene() {
+
+        // Create a quad covering the whole background and assign a texture to it where the
+        // Tango color camera contents will be rendered.
+        ScreenQuad backgroundQuad = new ScreenQuad();
+        Material tangoCameraMaterial = new Material();
+        tangoCameraMaterial.setColorInfluence(0);
+
+        // We need to use Rajawali's {@code StreamingTexture} since it sets up the texture
+        // for GL_TEXTURE_EXTERNAL_OES rendering
+        //https://forums.epicgames.com/threads/600758-Can-anyone-explain-what-is-texture-streaming-or-streaming-textures
+        mTangoCameraTexture =
+                new StreamingTexture("camera", (StreamingTexture.ISurfaceListener) null);
+        try {
+            tangoCameraMaterial.addTexture(mTangoCameraTexture);
+            backgroundQuad.setMaterial(tangoCameraMaterial);
+        } catch (ATexture.TextureException e) {
+            Log.e(TAG, "Exception creating texture for RGB camera contents", e);
+        }
+        getCurrentScene().addChildAt(backgroundQuad, 0);
+
         //setting up the light, should not change when using tango
         directionalLight = new DirectionalLight(1f, -3f, -1.0f);
         directionalLight.setColor(1.0f, 1.0f, 1.0f );
@@ -55,16 +93,65 @@ public class myRenderer extends RajawaliRenderer {
             Log.d("DEBUG","TEXTURE ERROR");
         }
 
-        earthSphere = new Sphere(1,24,24);
+        earthSphere = new Sphere(0.4f,20,20);
         earthSphere.setMaterial(material);
+        earthSphere.setPosition(0, 0, -3);
         getCurrentScene().addChild(earthSphere);
-        getCurrentCamera().setZ(4.2f);
+
+        //This doesn't seem to work with tango
+        //getCurrentCamera().setZ(3f);
     }
 
     @Override
     public void onRender(final long elapsedTime, final double deltaTime){
         super.onRender(elapsedTime, deltaTime);
         earthSphere.rotate(Vector3.Axis.Y, 1.0);
+    }
+
+    /**
+     * Update the scene camera based on the provided pose in Tango start of service frame.
+     * The device pose should match the pose of the device at the time the last rendered RGB
+     * frame, which can be retrieved with this.getTimestamp();
+     * NOTE: This must be called from the OpenGL render thread - it is not thread safe.
+     */
+    public void updateRenderCameraPose(TangoPoseData devicePose, DeviceExtrinsics extrinsics) {
+        Pose cameraPose = ScenePoseCalculator.toOpenGlCameraPose(devicePose, extrinsics);
+        getCurrentCamera().setRotation(cameraPose.getOrientation());
+        getCurrentCamera().setPosition(cameraPose.getPosition());
+    }
+
+    /**
+     * It returns the ID currently assigned to the texture where the Tango color camera contents
+     * should be rendered.
+     * NOTE: This must be called from the OpenGL render thread - it is not thread safe.
+     */
+    public int getTextureId() {
+        return mTangoCameraTexture == null ? -1 : mTangoCameraTexture.getTextureId();
+    }
+
+    /**
+     * We need to override this method to mark the camera for re-configuration (set proper
+     * projection matrix) since it will be reset by Rajawali on surface changes.
+     */
+    @Override
+    public void onRenderSurfaceSizeChanged(GL10 gl, int width, int height) {
+        super.onRenderSurfaceSizeChanged(gl, width, height);
+        mSceneCameraConfigured = false;
+    }
+
+    public boolean isSceneCameraConfigured() {
+        return mSceneCameraConfigured;
+    }
+
+    /**
+     * Sets the projection matrix for the scen camera to match the parameters of the color camera,
+     * provided by the {@code TangoCameraIntrinsics}.
+     */
+    public void setProjectionMatrix(TangoCameraIntrinsics intrinsics) {
+        Matrix4 projectionMatrix = ScenePoseCalculator.calculateProjectionMatrix(
+                intrinsics.width, intrinsics.height,
+                intrinsics.fx, intrinsics.fy, intrinsics.cx, intrinsics.cy);
+        getCurrentCamera().setProjectionMatrix(projectionMatrix);
     }
 
     @Override
